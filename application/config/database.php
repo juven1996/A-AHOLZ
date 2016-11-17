4bb100dcf61c9500b3711a7da27e16654f2c7ed8
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-11-16 19:37:48 -0800

    change database config

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'squeal';
-$db['default']['database'] = 'ci_sample';
+$db['default']['password'] = 'secret';
+$db['default']['database'] = 'ci_db';
 
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
