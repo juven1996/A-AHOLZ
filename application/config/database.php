a99b1aaaa814901d9c7e62d053a7bb633de5fbb6
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-11-16 19:26:41 -0800

    use the mysqli db connection driver instead

--- application/config/database.php
+++ application/config/database.php
@@ -53,7 +53,7 @@ $db['default']['username'] = 'root';
 $db['default']['password'] = 'squeal';
 $db['default']['database'] = 'ci_sample';
 
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
