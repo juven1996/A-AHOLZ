203f501066f0e20a6b90e2ec244beaae4f00ede8
juven1996@gmail.com <juven1996@gmail.com>
2016-11-21 09:29:01 -0800

    created forms for the order page

--- application/controllers/admin_order.php
+++ application/controllers/admin_order.php
@@ -17,12 +17,7 @@ class Admin_order extends CI_Controller {
  
     
     public function index()
-    {
-
-        //all the posts sent by the view
-        $search_string = $this->input->post('search_string');        
-        $order = $this->input->post('order'); 
-        $order_type = $this->input->post('order_type'); 
+    {     
 
         //pagination settings
         $config['per_page'] = 5;
@@ -46,181 +41,11 @@ class Admin_order extends CI_Controller {
             $limit_end = 0;
         } 
 
-        //if order type was changed
-        if($order_type){
-            $filter_session_data['order_type'] = $order_type;
-        }
-        else{
-            //we have something stored in the session? 
-            if($this->session->userdata('order_type')){
-                $order_type = $this->session->userdata('order_type');    
-            }else{
-                //if we have nothing inside session, so it's the default "Asc"
-                $order_type = 'Asc';    
-            }
-        }
-        //make the data type var avaible to our view
-        $data['order_type_selected'] = $order_type;        
-
-
-        //we must avoid a page reload with the previous session data
-        //if any filter post was sent, then it's the first time we load the content
-        //in this case we clean the session filter data
-        //if any filter post was sent but we are in some page, we must load the session data
-
-        //filtered && || paginated
-        if($search_string !== false && $order !== false || $this->uri->segment(3) == true){ 
-           
-            
-            if($search_string){
-                $filter_session_data['search_string_selected'] = $search_string;
-            }else{
-                $search_string = $this->session->userdata('search_string_selected');
-            }
-            $data['search_string_selected'] = $search_string;
-
-            if($order){
-                $filter_session_data['order'] = $order;
-            }
-            else{
-                $order = $this->session->userdata('order');
-            }
-            $data['order'] = $order;
-
-            //save session data into the session
-            if(isset($filter_session_data)){
-              $this->session->set_userdata($filter_session_data);    
-            }
-            
-            //fetch sql data into arrays
-            $data['count_products']= $this->order_model->count_order($search_string, $order);
-            $config['total_rows'] = $data['count_products'];
-
-            //fetch sql data into arrays
-            if($search_string){
-                if($order){
-                    $data['order'] = $this->order_model->get_order($search_string, $order, $order_type, $config['per_page'],$limit_end);        
-                }else{
-                    $data['order'] = $this->order_model->get_order($search_string, '', $order_type, $config['per_page'],$limit_end);           
-                }
-            }else{
-                if($order){
-                    $data['order'] = $this->order_model->get_order('', $order, $order_type, $config['per_page'],$limit_end);        
-                }else{
-                    $data['order'] = $this->order_model->get_order('', '', $order_type, $config['per_page'],$limit_end);        
-                }
-            }
-
-        }else{
-
-            //clean filter data inside section
-            $filter_session_data['manufacture_selected'] = null;
-            $filter_session_data['search_string_selected'] = null;
-            $filter_session_data['order'] = null;
-            $filter_session_data['order_type'] = null;
-            $this->session->set_userdata($filter_session_data);
-
-            //pre selected options
-            $data['search_string_selected'] = '';
-            $data['order'] = 'id';
-
-            //fetch sql data into arrays
-            $data['count_products']= $this->order_model->count_order();
-            $data['order'] = $this->order_model->get_order('', '', $order_type, $config['per_page'],$limit_end);        
-            $config['total_rows'] = $data['count_products'];
-
-        }//!isset($search_string) && !isset($order)
-         
-        //initializate the panination helper 
-        $this->pagination->initialize($config);   
-
         //load the view
         $data['main_content'] = 'admin/order/list';
         $this->load->view('includes/template', $data);  
 
     }//index
 
-    public function add()
-    {
-        //if save button was clicked, get the data sent via post
-        if ($this->input->server('REQUEST_METHOD') === 'POST')
-        {
-
-            //form validation
-            $this->form_validation->set_rules('name', 'name', 'required');
-            $this->form_validation->set_error_delimiters('<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a><strong>', '</strong></div>');
-            
-
-            //if the form has passed through the validation
-            if ($this->form_validation->run())
-            {
-                $data_to_store = array(
-                    'name' => $this->input->post('name'),
-                );
-                //if the insert has returned true then we show the flash message
-                if($this->order_model->store_manufacture($data_to_store)){
-                    $data['flash_message'] = TRUE; 
-                }else{
-                    $data['flash_message'] = FALSE; 
-                }
-
-            }
-
-        }
-        //load the view
-        $data['main_content'] = 'admin/order/add';
-        $this->load->view('includes/template', $data);  
-    }       
-
-    
-    public function update()
-    {
-        //product id 
-        $id = $this->uri->segment(4);
-  
-        //if save button was clicked, get the data sent via post
-        if ($this->input->server('REQUEST_METHOD') === 'POST')
-        {
-            //form validation
-            $this->form_validation->set_rules('name', 'name', 'required');
-            $this->form_validation->set_error_delimiters('<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a><strong>', '</strong></div>');
-            //if the form has passed through the validation
-            if ($this->form_validation->run())
-            {
-    
-                $data_to_store = array(
-                    'name' => $this->input->post('name'),
-                );
-                //if the insert has returned true then we show the flash message
-                if($this->order_model->update_manufacture($id, $data_to_store) == TRUE){
-                    $this->session->set_flashdata('flash_message', 'updated');
-                }else{
-                    $this->session->set_flashdata('flash_message', 'not_updated');
-                }
-                redirect('admin/order/update/'.$id.'');
-
-            }//validation run
-
-        }
-
-        //if we are updating, and the data did not pass trough the validation
-        //the code below wel reload the current data
-
-        //product data 
-        $data['manufacture'] = $this->order_model->get_manufacture_by_id($id);
-        //load the view
-        $data['main_content'] = 'admin/order/edit';
-        $this->load->view('includes/template', $data);            
-
-    }//update
-
-    
-    public function delete()
-    {
-        //product id 
-        $id = $this->uri->segment(4);
-        $this->order_model->delete_manufacture($id);
-        redirect('admin/order');
-    }//edit
 
 }
--- application/views/admin/order/list.php
+++ application/views/admin/order/list.php
@@ -1,86 +1,73 @@
-    <div class="container top">
+<div class="container top">
 
-      <ul class="breadcrumb">
+    <ul class="breadcrumb">
         <li>
-          <a href="<?php echo site_url("admin"); ?>">
-            <?php echo ucfirst($this->uri->segment(1));?>
-          </a> 
-          <span class="divider">/</span>
+            <a href="<?php echo site_url("admin"); ?>">
+                <?php echo ucfirst($this->uri->segment(1)); ?>
+            </a> 
+            <span class="divider">/</span>
         </li>
         <li class="active">
-          <?php echo ucfirst($this->uri->segment(2));?>
+            <?php echo ucfirst($this->uri->segment(2)); ?>
         </li>
-      </ul>
+    </ul>
+    <div class="col-md-12">
+        <div class="jumbotron">
+            <h2>
+                Create your order here.
+            </h2>
+            <p>
+                Please input the specifications of the cabinet you want.
+            </p>
+        </div>
+        <form role="form" class="form-inline">
+            <div class="form-group">
 
-      <div class="page-header users-header">
-        <h2>
-          <?php echo ucfirst($this->uri->segment(2));?> 
-          <a  href="<?php echo site_url("admin").'/'.$this->uri->segment(2); ?>/add" class="btn btn-success">Add a new</a>
-        </h2>
-      </div>
-      
-      <div class="row">
-        <div class="span12 columns">
-          <div class="well">
-           
-            <?php
-           
-            $attributes = array('class' => 'form-inline reset-margin', 'id' => 'myform');
-           
-            //save the columns names in a array that we will use as filter         
-            $options_order = array();    
-            foreach ($order as $array) {
-              foreach ($array as $key => $value) {
-                $options_order[$key] = $key;
-              }
-              break;
-            }
+                <label for="Width">
+                    Width
+                </label>
+                <input type="text" class="form-control" id="widthText" />
+            </div>
+            <div class="form-group">
 
-            echo form_open('admin/order', $attributes);
-     
-              echo form_label('Search:', 'search_string');
-              echo form_input('search_string', $search_string_selected);
+                <label for="Height">
+                    Height
+                </label>
+                <input type="text" class="form-control" id="heightText" />
+            </div>
+            <div class="form-group">
 
-              echo form_label('Order by:', 'order');
-              echo form_dropdown('order', $options_order, $order, 'class="span2"');
+                <label for="Depth">
+                    Depth
+                </label>
+                <input type="text" class="form-control" id="depthText" />
+            </div>
+            <div class="form-group">
 
-              $data_submit = array('name' => 'mysubmit', 'class' => 'btn btn-primary', 'value' => 'Go');
+                <label for="Hinges">
+                    Number of Hinges
+                </label>
+                <input type="text" class="form-control" id="hingesText" />
+            </div>
+            <div class="form-group">
 
-              $options_order_type = array('Asc' => 'Asc', 'Desc' => 'Desc');
-              echo form_dropdown('order_type', $options_order_type, $order_type_selected, 'class="span1"');
-
-              echo form_submit($data_submit);
-
-            echo form_close();
-            ?>
-
-          </div>
-
-          <table class="table table-striped table-bordered table-condensed">
-            <thead>
-              <tr>
-                <th class="header">#</th>
-                <th class="yellow header headerSortDown">Name</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              foreach($order as $row)
-              {
-                echo '<tr>';
-                echo '<td>'.$row['id'].'</td>';
-                echo '<td>'.$row['name'].'</td>';
-                echo '<td class="crud-actions">
-                  <a href="'.site_url("admin").'/order/update/'.$row['id'].'" class="btn btn-info">view & edit</a>  
-                  <a href="'.site_url("admin").'/order/delete/'.$row['id'].'" class="btn btn-danger">delete</a>
-                </td>';
-                echo '</tr>';
-              }
-              ?>      
-            </tbody>
-          </table>
-
-          <?php echo '<div class="pagination">'.$this->pagination->create_links().'</div>'; ?>
-
-      </div>
+                <label for="Doors">
+                    Number of Doors
+                </label>
+                <input type="text" class="form-control" id="doorsText" />
+            </div>
+            <ul class="dropdown-menu">
+                <li>
+                    <a href="#">Finish Left</a>
+                </li>
+                <li class="disabled">
+                    <a href="#">Finish Right</a>
+                </li>
+            </ul>
+            <button type="submit" class="btn btn-default">
+                Submit
+            </button>
+        </form>
     </div>
+
+</div>
